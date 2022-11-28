082f817f0f96212e9416c7e7859ac01b94131bd8
Krys <75592214+Kooties@users.noreply.github.com>
2022-11-27 19:54:09 -0500

    Finished Day 4

--- 2021/Day4/bingo.ps1
+++ 2021/Day4/bingo.ps1
@@ -25,7 +25,7 @@ for($i=2; $i -lt $data.length; $i++){
         $3split = foreach($number in $3){([int]::parse($number))}
         $4split = foreach($number in $4){([int]::parse($number))}
         $5split = foreach($number in $5){([int]::parse($number))}
-        Write-Host "Writing Card #" + ((($i-2)/6)+1)
+        #Write-Host "Writing Card #" + ((($i-2)/6)+1)
         $card = [bingoCard]@{
             row1 = $1split
             row2 = $2split
@@ -38,5 +38,52 @@ for($i=2; $i -lt $data.length; $i++){
 }
 
 foreach($number in $calledNumbers){
-    
+    foreach($card in $allCards){
+        if($card.row1[0] -is [int]){
+        for($j=0;$j -lt 5;$j++){
+            if($card.row1[$j] -eq $number){
+                $card.row1[$j] = 0
+                #Write-Host "Match Detected"
+            }
+            if($card.row2[$j] -eq $number){
+                $card.row2[$j] = 0
+                #Write-Host "Match Detected"
+            }
+            if($card.row3[$j] -eq $number){
+                $card.row3[$j] = 0
+                #Write-Host "Match Detected"
+            }
+            if($card.row4[$j] -eq $number){
+                $card.row4[$j] = 0
+                #Write-Host "Match Detected"
+            }
+            if($card.row5[$j] -eq $number){
+                $card.row5[$j] = 0
+                #Write-Host "Match Detected"
+            }
+        }
+        $row1total = $card.row1[0] + $card.row1[1] + $card.row1[2] + $card.row1[3] + $card.row1[4]
+        $row2total = $card.row2[0] + $card.row2[1] + $card.row2[2] + $card.row2[3] + $card.row2[4]
+        $row3total = $card.row3[0] + $card.row3[1] + $card.row3[2] + $card.row3[3] + $card.row3[4]
+        $row4total = $card.row4[0] + $card.row4[1] + $card.row4[2] + $card.row4[3] + $card.row4[4]
+        $row5total = $card.row5[0] + $card.row5[1] + $card.row5[2] + $card.row5[3] + $card.row5[4]
+
+        if(($row1total -eq 0) -or ($row2total -eq 0) -or ($row3total -eq 0) -or ($row4total -eq 0) -or ($row5total -eq 0)){
+            $sum = $row1total + $row2total + $row3total + $row4total + $row5total
+            $winningNumber = $sum*$number
+            Write-Host "$card`n$sum x $number = $winningNumber"
+            $card.row1[0] = "x"
+        }
+
+        for($k=0;$k -lt 5;$k++){
+            $rowTotal = $card.row1[$k] + $card.row2[$k] +$card.row3[$k] +$card.row4[$k] +$card.row5[$k]
+            if($rowTotal -eq 0){
+                $sum = $row1total + $row2total + $row3total + $row4total + $row5total
+                $winningNumber = $sum*$number
+                Write-Host $card.row1 `n $card.row2 `n $card.row3 `n $card.row4 `n $card.row5 `n "`n$sum x $number = $winningNumber"
+                $card.row1[0] = "x"
+            }
+        }
+    }
+}
 }
