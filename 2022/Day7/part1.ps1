5dedc23ad18b9af32e667285400b60c3ec728b1e
Krys <75592214+Kooties@users.noreply.github.com>
2022-12-08 17:01:16 -0500

    Trying Day 7; not yet complete

---
+++ 2022/Day7/part1.ps1
@@ -0,0 +1,21 @@
+$rawData = Get-Content testinput.txt
+$maxSize = 100000
+$fileRegex = '[\d]{1,}'
+$dname = ""
+$numUp = 0
+$currentDir = ""
+foreach($line in $rawData){
+    $split = $line.split(' ')
+    switch -wildcard ($line){
+        
+    }
+}
+
+
+
+
+
+
+$directory.clear()
+$cacheList.clear()
+$directories.clear()
