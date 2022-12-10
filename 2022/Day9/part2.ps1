bddaad108901f204257ce257bd6685af5e953ce1
Krys <75592214+Kooties@users.noreply.github.com>
2022-12-10 00:56:28 -0500

    working on part 2, not done

--- 2022/Day9/part2.ps1
+++ 2022/Day9/part2.ps1
@@ -2,17 +2,21 @@ class Knot{
     [int]$x
     [int]$y
     [void]MoveRope([string]$dir,[int]$num,[int]$hx,[int]$hy,[System.Collections.ArrayList]$spots){
+        Write-Host "Into method:"
         $tx = $this.x
         $ty = $this.y
         switch($dir){
             {$_ -match "R"}{
+                Write-Host "Match R:"
                 #$hx += $num
                 $lrDistance = ($hx-$this.x)
                 $udDistance = ($hy-$this.y)
+                Write-Host "Hori distance is $lrDistance; Vert is $udDistance"
                 for($i=0; $i -lt $num; $i++){
                     $lrDistance = ($hx-$this.x)
                     $udDistance = ($hy-$this.y)
                     if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
+                        Write-Host "Time to move!"
                         if(($lrDistance -gt 1)){
                             $this.x++
                             $this.y = $hy
@@ -24,6 +28,7 @@ class Knot{
                     $hcurrentPoint = "$hx,$hy"
                     $tx = $this.x
                     $ty = $this.y
+                    Write-Host "Moved to $tx,$ty"
                     $tcurrentPoint = "$tx,$ty"
                     if(!$spots.Contains($tcurrentPoint)){
                         $null = $spots.Add($tcurrentPoint)
@@ -33,13 +38,16 @@ class Knot{
                 break
             }#end of switch case
             {$_ -match "L"}{
+                Write-Host "Match L:"
                 #$hx -= $num
                 $lrDistance = ($hx-$this.x)
                 $udDistance = ($hy-$this.y)
+                Write-Host "Hori distance is $lrDistance; Vert is $udDistance"
                 for($i=0; $i -lt $num; $i++){
                     $lrDistance = ($hx-$this.x)
                     $udDistance = ($hy-$this.y)
                     if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
+                        Write-Host "Time to move!"
                         if(($lrDistance -gt 1)){
                             $this.x++
                             $this.y = $hy
@@ -51,6 +59,7 @@ class Knot{
                     $hcurrentPoint = "$hx,$hy"
                     $tx = $this.x
                     $ty = $this.y
+                    Write-Host "Moved to $tx,$ty"
                     $tcurrentPoint = "$tx,$ty"
                     if(!$spots.Contains($tcurrentPoint)){
                         $null = $spots.Add($tcurrentPoint)
@@ -60,13 +69,16 @@ class Knot{
                 break
             }#end of switch case
             {$_ -match "U"}{
+                Write-Host "Match U:"
                 #$hy += $num
                 $lrDistance = ($hx-$tx)
                 $udDistance = ($hy-$ty)
+                Write-Host "Hori distance is $lrDistance; Vert is $udDistance"
                 for($i=0; $i -lt $num; $i++){
                     $lrDistance = ($hx-$this.x)
                     $udDistance = ($hy-$this.y)
                     if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
+                        Write-Host "Time to move!"
                         if(($udDistance -gt 1)){
                             $this.y++
                             $this.x = $hx
@@ -78,6 +90,7 @@ class Knot{
                     $hcurrentPoint = "$hx,$hy"
                     $tx = $this.x
                     $ty = $this.y
+                    Write-Host "Moved to $tx,$ty"
                     $tcurrentPoint = "$tx,$ty"
                     if(!$spots.Contains($tcurrentPoint)){
                         $null = $spots.Add($tcurrentPoint)
@@ -87,13 +100,16 @@ class Knot{
                 break
             }#end of switch case
             {$_ -match "D"}{
+                Write-Host "Match D:"
                 #$hy -= $num
                 $lrDistance = ($hx-$this.x)
                 $udDistance = ($hy-$this.y)
+                Write-Host "Hori distance is $lrDistance; Vert is $udDistance"
                 for($i=0; $i -lt $num; $i++){
                     $lrDistance = ($hx-$this.x)
                     $udDistance = ($hy-$this.y)
                     if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
+                        Write-Host "Time to move!"
                         if(($udDistance -gt 1)){
                             $this.y++
                             $this.x = $hx
@@ -105,6 +121,7 @@ class Knot{
                     $hcurrentPoint = "$hx,$hy"
                     $tx = $this.x
                     $ty = $this.y
+                    Write-Host "Moved to $tx,$ty"
                     $tcurrentPoint = "$tx,$ty"
                     if(!$spots.Contains($tcurrentPoint)){
                         $null = $spots.Add($tcurrentPoint)
@@ -118,121 +135,6 @@ class Knot{
     }
 }
 
-<#function Move-Rope{
-    [CmdletBinding()]
-    param(
-        [ValidateNotNullOrEmpty()]
-        [string]$dir,
-        [int]$num,
-        [int]$hx,
-        [int]$hy,
-        [int]$tx,
-        [int]$ty,
-        [System.Collections.ArrayList]$ropevisitedSpots
-    )
-    switch($dir){
-        {$_ -match "R"}{
-            #$hx += $num
-            $lrDistance = ($hx-$tx)
-            $udDistance = ($hy-$ty)
-            for($i=0; $i -lt $num; $i++){
-                $lrDistance = ($hx-$tx)
-                $udDistance = ($hy-$ty)
-                if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
-                    if(($lrDistance -gt 1)){
-                        $tx++
-                        $ty = $hy
-                    }elseif($lrDistance -lt -1){
-                        $tx--
-                        $ty = $hy
-                    }
-                }
-                $hcurrentPoint = "$hx,$hy"
-                $tcurrentPoint = "$tx,$ty"
-                if(!$ropevisitedSpots.Contains($tcurrentPoint)){
-                    $null = $ropevisitedSpots.Add($tcurrentPoint)
-                }
-                #Write-Host "H moved to $hx,$hy; T moved to $tx,$ty"
-            }
-            break
-        }#end of switch case
-        {$_ -match "L"}{
-            #$hx -= $num
-            $lrDistance = ($hx-$tx)
-            $udDistance = ($hy-$ty)
-            for($i=0; $i -lt $num; $i++){
-                $lrDistance = ($hx-$tx)
-                $udDistance = ($hy-$ty)
-                if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
-                    if(($lrDistance -gt 1)){
-                        $tx++
-                        $ty = $hy
-                    }elseif($lrDistance -lt -1){
-                        $tx--
-                        $ty = $hy
-                    }
-                }
-                $hcurrentPoint = "$hx,$hy"
-                $tcurrentPoint = "$tx,$ty"
-                if(!$ropevisitedSpots.Contains($tcurrentPoint)){
-                    $null = $ropevisitedSpots.Add($tcurrentPoint)
-                }
-                #Write-Host "H moved to $hx,$hy; T moved to $tx,$ty"
-            }
-            break
-        }#end of switch case
-        {$_ -match "U"}{
-            #$hy += $num
-            $lrDistance = ($hx-$tx)
-            $udDistance = ($hy-$ty)
-            for($i=0; $i -lt $num; $i++){
-                $lrDistance = ($hx-$tx)
-                $udDistance = ($hy-$ty)
-                if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
-                    if(($udDistance -gt 1)){
-                        $ty++
-                        $tx = $hx
-                    }elseif($udDistance -lt -1){
-                        $ty--
-                        $tx = $hx
-                    }
-                }
-                $hcurrentPoint = "$hx,$hy"
-                $tcurrentPoint = "$tx,$ty"
-                if(!$ropevisitedSpots.Contains($tcurrentPoint)){
-                    $null = $ropevisitedSpots.Add($tcurrentPoint)
-                }
-                #Write-Host "H moved to $hx,$hy; T moved to $tx,$ty"
-            }
-            break
-        }#end of switch case
-        {$_ -match "D"}{
-            #$hy -= $num
-            $lrDistance = ($hx-$tx)
-            $udDistance = ($hy-$ty)
-            for($i=0; $i -lt $num; $i++){
-                $lrDistance = ($hx-$tx)
-                $udDistance = ($hy-$ty)
-                if(([Math]::Abs($lrDistance) + ([Math]::Abs($udDistance))) -gt 1){
-                    if(($udDistance -gt 1)){
-                        $ty++
-                        $tx = $hx
-                    }elseif($udDistance -lt -1){
-                        $ty--
-                        $tx = $hx
-                    }
-                }
-                $hcurrentPoint = "$hx,$hy"
-                $tcurrentPoint = "$tx,$ty"
-                if(!$ropevisitedSpots.Contains($tcurrentPoint)){
-                    $null = $ropevisitedSpots.Add($tcurrentPoint)
-                }
-                #Write-Host "H moved to $hx,$hy; T moved to $tx,$ty"
-            }
-            break
-        }#end of switch case
-    }
-}#>
 
 $rawData = Get-Content test2input.txt
 $rope1 = [Knot]::new()
@@ -255,14 +157,15 @@ $r7array = [System.Collections.ArrayList]@()
 $r8array = [System.Collections.ArrayList]@()
 $r9array = [System.Collections.ArrayList]@()
 $arrays = $r1array,$r2array,$r3array,$r4array,$r5array,$r6array,$r7array,$r8array,$r9array
-
+$i=0
 foreach($rope in $ropeKnots){
     $rope.x = "1"
     $rope.y = "1"
     $x=1
     $y=1
     $currentPoint = "$x,$y"
-    $null=$rope.VisitedSpots.Add($currentPoint)
+    $null=$arrays[$i].Add($currentPoint)
+    $i++
 }
 
 foreach($line in $rawData){
@@ -271,27 +174,53 @@ foreach($line in $rawData){
     $num = $split[1]
     $i=0
     foreach($rope in $ropeKnots){
+        $hori = $rope.x
+        $vert = $rope.y
+        Write-Host "Knot $i is starting at $hori,$vert"
         if($i -eq 0){
+            "First knot in rope"
             switch($dir){
                 {$_ -match "R"}{
                     $rope.x += $num
+                    $x = $rope.x
+                    $y = $rope.y
+                    Write-Host "First knot moved $num spaces to $x,$y"
                 }
                 {$_ -match "L"}{
                     $rope.x -= $num
+                    $x = $rope.x
+                    $y = $rope.y
+                    Write-Host "First knot moved $num spaces to $x,$y"
                 }
                 {$_ -match "U"}{
                     $rope.y += $num
+                    $x = $rope.x
+                    $y = $rope.y
+                    Write-Host "First knot moved $num spaces to $x,$y"
                 }
                 {$_ -match "D"}{
                     $rope.y -= $num
+                    $x = $rope.x
+                    $y = $rope.y
+                    Write-Host "First knot moved $num spaces to $x,$y"
                 }
+                
             }
+            $hori = $rope.x
+            $vert = $rope.y
+            Write-Host "Knot ended at $hori,$vert"
             $i++
         }else{
+            Write-Host "Not first knot in rope [he he he]"
             $hx = $rope[$i-1].x
             $hy = $rope[$i-1].y
             $rope.MoveRope($dir, $num, $hx, $hy,$arrays[$i])
+            $hori = $rope.x
+            $vert = $rope.y
+            Write-Host "Knot ended at $hori,$vert"
             $i++
         }
     }
 }
+
+return $r9array.count
