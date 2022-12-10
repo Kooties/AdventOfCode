57368ad33d425e24cbf92f5d8409dac6702bcf50
Krys <75592214+Kooties@users.noreply.github.com>
2022-12-10 01:27:53 -0500

    Took out Write-Hosts to check time

--- 2022/Day10/part1.ps1
+++ 2022/Day10/part1.ps1
@@ -7,33 +7,26 @@ $g = 1
 foreach($line in $rawData){
     $data = $line.split(" ")
     $command = [string]$data[0]
-    Write-Host "$g line is $line"
     switch($command){
         {$_.StartsWith("addx")}{
             $number = [int]$data[1]
-            Write-Host "Adding $number to x"
             $cycle++
             if(($cycle -eq 20) -or (($cycle-20)%$cycleNumber -eq 0)){
                 $currentSum += ($x*$cycle)
-                Write-Host "Cycle $cycle x value is $x; thing is $thing; to make the current sum $currentSum"
             }
             $cycle++
             $x += $number
             if(($cycle -eq 20) -or (($cycle-20)%$cycleNumber -eq 0)){
                 $currentSum += ($x*$cycle)
                 $thing = ($x*$cycle)
-                Write-Host "Cycle $cycle x value is $x; thing is $thing; to make the current sum $currentSum"
             }
-            Write-Host "Value of x is $x"
         }
         {$_.StartsWith("noop")}{
             $cycle++
             if(($cycle -eq 20) -or (($cycle-20)%$cycleNumber -eq 0)){
                 $currentSum += ($x*$cycle)
                 $thing = ($x*$cycle)
-                Write-Host "Cycle $cycle x value is $x; thing is $thing; to make the current sum $currentSum"
             }
-            Write-Host "Value of x is $x"
         }
     }
     $g++
