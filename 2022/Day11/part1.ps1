37035e8466f449283fed753885d2c00f9d86a0b4
Krys <75592214+Kooties@users.noreply.github.com>
2022-12-11 18:56:45 -0500

    please tell me what i'm doing wrong plz thx

--- 2022/Day11/part1.ps1
+++ 2022/Day11/part1.ps1
@@ -1,11 +1,11 @@
-Class Monkey{
-    $inventory = [System.Collections.ArrayList]@()
-    $operand = ''
-    $num = ''
+class Monkey{
     [int]$divisor
     [int]$trueMonkey
     [int]$falseMonkey 
-
+    [int]$numInspections
+    [System.Collections.ArrayList]$inventory
+    [string]$operand
+    [string]$num
     [int]DoFunction([int]$iteration){
         if(($this.num).StartsWith("old")){
             $this.num = $this.inventory[$iteration]
@@ -25,27 +25,22 @@ Class Monkey{
                 $result = ($this.inventory[$iteration] - [int] $this.num)
             }
         }
+        $this.numInspections ++
         return $result
     }
-
     [void]throwthing([int]$thing,[int]$monkeyNumber,[System.Collections.ArrayList]$monkeys){
-        $null = $monkeys[$monkeyNumber].Inventory.Add($thing)
+        $null = $monkeys[$monkeyNumber].Inventory.Add([int]$thing/3)
         $index = $this.inventory.IndexOf($thing)
         $this.inventory[$index] = 0
     }
     [void]fixinventory(){
         $this.inventory.Remove(0)
     }
+    [void]AddInventory([int]$thing){
+        $this.inventory.Add($thing)
+    }
 }
 
-function Get-PostInspectionWorry{
-    [CmdletBinding()]
-    param(
-        [ValidateNotNullOrEmpty()]
-        [int]$worryLevel
-    )
-    return [int]($worryLevel/3)
-}
 
 $rawData = Get-Content testinput.txt
 $rounds = 20
@@ -55,19 +50,25 @@ for($i=0; $i -lt 7; $i++){
     $newMonkey = [Monkey]::New()
     $null = $monkeys.add($newMonkey)
 }
+$iteration = 0
 
 foreach($line in $rawData){
-    $string = ""
     if($line.StartsWith("Monkey:")){
         $split = $line.split(" ")
-        $iteration = [int]$split[1][0]
+        $iteration = ([int]$split[1][0] -48)
     }elseif($line.StartsWith("Starting items:")){
         $firstSplit = $line.split(' ')
-        $numbers = $firstSplit[2..($firstSplit.length-1)]
-        foreach($number in $numbers){
-            $null = $monkeys[$iteration].inventory.add($number)
+        foreach($thing in $firstSplit){
+            $number = 0
+            if(!($thing.StartsWith("Starting")) -and !($thing.StartsWith("items"))){
+                if($thing -match ','){
+                    $number = [int]$thing.substring(0,$thing.length)
+                }else{
+                    $number = [int]$thing
+                }
+                $monkeys[$iteration].AddInventory($number)
+            }
         }
-
     }elseif($line.startswith("Operation")){
         $split = $line.split(" ") #4 and 5 are operand and number, respectively
         $monkeys[$iteration].Operand = $split[4]
@@ -81,5 +82,8 @@ foreach($line in $rawData){
     }elseif($line.Startswith("If false")){
         $split = $line.split(" ")
         $monkeys[$iteration].falsemonkey = $split[5]
+    }else{
     }
 }
+
+return $monkeys
