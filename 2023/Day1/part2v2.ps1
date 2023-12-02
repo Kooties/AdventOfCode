5ad447723b798dbc9c016592d70115733d56bdbf
Krys <75592214+Kooties@users.noreply.github.com>
2023-12-01 23:35:04 -0500

    Finished day 1; created files for part 1 of day 2

--- 2023/Day1/part2v2.ps1
+++ 2023/Day1/part2v2.ps1
@@ -1,20 +1,22 @@
-$rawData = get-content test2.txt
+$rawData = get-content Input.txt
 $total = 0
-#$line = "one2threeight"
 $values = "one","two","three","four","five","six","seven","eight","nine","1","2","3","4","5","6","7","8","9"
 
-$rawData = "two1nine"
-$rawData = "7one2threeight"
 foreach($line in $rawData){
     $numArray = [System.Collections.ArrayList]@()
-    [int]$indexOfFirst = -2
-    [int]$indexOfLast= -2
+    [int]$indexOfFirst = 567
+    [int]$indexOfLast= 567
     $val = ""
+    Write-Host "Checking $line"
     foreach($value in $values){
         Write-Host "checking for $value in string $line"
-        if(($index = $line.IndexOf($value)) -and ($index -ge 0)){
+        #Write-Host "Current minIndex is $indexOfFirst and current maxIndex is $indexOfLast"
+        #$index = $line.IndexOf($value) | Out-Null
+        if(($line.IndexOf($value)) -gt -1){
+            $index = $line.IndexOf($value)
+            if($index -gt -1){
             Write-Host "Found at index $index"
-            if($indexOfFirst -eq -2){
+            if($indexOfFirst -eq 567){
                 Write-Host "No previous match; setting initial values"
                 $indexOfFirst = $index
                 $indexOfLast = $index
@@ -93,12 +95,16 @@ foreach($line in $rawData){
                 $numArray[0] = $val
                 Write-Host "numArray is currently $numArray"
             }
-        }
+        }}
     }
     $numString = ""
     foreach($num in $numArray){
         $numString += [string]$num
     }
+    Write-Host "Number for line is $numString"
+    Write-Host "Old total is $total"
     $total += [int]$numString
+    Write-Host "New Total is $total"
+    $numArray.clear()
 }
 
