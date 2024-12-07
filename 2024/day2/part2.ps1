120ddead190a2d3f44179133874de28b02b55828
Krys <75592214+Kooties@users.noreply.github.com>
2024-12-07 14:02:10 -0500

    Going back to day 2 part 2; making progress

--- 2024/day2/part2.ps1
+++ 2024/day2/part2.ps1
@@ -1,25 +1,145 @@
-$rawData = Get-Content example.txt
- <#Safe reports are lines where the numbers are either increasing or decreasing by between 1 and 3 through the whole line#>
-$safeLines = 0
-
-foreach($line in $rawData){
+Function Compare-Line([string]$line){
+    #Write-Host "Parsing next line"
+    $lastNumber = 0
+    $nextNumber = 0
+    $allowedErrors = 1
     $reports = $line.split(" ")
-    $Safe = $true
-    [bool]$increasing
-    [bool]$decreasing
-    $errorCount = 0
-    $count = $reports.count
-    foreach($report in $reports){
-        $regex = $report
-        if($regex.matches($reports).count -gt 1){
-            $safe = $false
-        }elseif($regex.matches($reports).count -eq 1){
-            
+    $isSafe = $true
+    $increasing = $false
+    $decreasing = $false
+   $i = 0
+   while(($i -lt ($reports.length)) -and ($isSafe)){
+    #Write-Host "On value $i"
+    if($i -eq 0){
+        $lastNumber = [int]$reports[$i]
+        $i++
+        #"Value 1 of line is $lastNumber "
+    }elseif($i -eq 1){
+        #Write-Host "Parsing second value"
+        $nextNumber = [int]$reports[$i]
+        #Write-Host "Last value is $lastNumber"
+        #Write-Host "Next value is $nextNumber"
+        if($nextNumber -gt $lastNumber){
+            #Write-Host "Line is increasing"
+            $increasing = $true
+            $decreasing = $false
+            if([Math]::Abs($nextNumber - $lastNumber) -gt 3){
+                if($allowedErrors -ne 0){
+                    $allowedErrors--
+                }else{
+                    $isSafe = $false
+                }
+                #Write-Host "Jump of more than 3"
+                
+            }
+        }elseif($nextNumber -lt $lastNumber){
+            #Write-Host "Line is decreasing"
+            $decreasing = $true
+            $increasing = $false
+            if([Math]::Abs($nextNumber - $lastNumber) -gt 3){
+                #Write-Host "Jump of more than 3"
+                if($allowedErrors -ne 0){
+                    $allowedErrors--
+                }else{
+                    $isSafe = $false
+                }
+            }
+        }elseif($nextNumber -eq $lastNumber){
+            #Write-Host "Numbers are equal"
+            if($allowedErrors -ne 0){
+                $allowedErrors--
+            }else{
+                $isSafe = $false
+            }
+        }
+        $lastNumber = [int]$reports[$i]
+        $i++
+    }elseif($increasing){
+        #Write-Host "increasing elseif"
+        $nextNumber = [int]$reports[$i]
+        #Write-Host "next number is $nextNumber"
+        if($nextNumber -le $lastNumber){
+            if($allowedErrors -ne 0){
+                $allowedErrors--
+            }else{
+                $isSafe = $false
+            }
+        }elseif([Math]::Abs($nextNumber - $lastNumber) -gt 3){
+            #Write-Host "Jump of more than 3"
+            if($allowedErrors -ne 0){
+                $allowedErrors--
+            }else{
+                $isSafe = $false
+            }
+        }elseif($i -eq ($reports.length - 1)){
+            #Write-Host "Line is safe"
+            $safeLines ++
+            $i = 50
+        }else{
+            $lastNumber = [int]$reports[$i]
+            $i++
+        }
+    }elseif($decreasing){
+        #Write-Host "decreasing elseif"
+        $nextNumber = [int]$reports[$i]
+        #Write-Host "next number is $nextNumber"
+        if($nextNumber -ge $lastNumber){
+            if($allowedErrors -ne 0){
+                $allowedErrors--
+            }else{
+                $isSafe = $false
+            }
+        }elseif([Math]::Abs($nextNumber - $lastNumber) -gt 3){
+            if($allowedErrors -ne 0){
+                $allowedErrors--
+            }else{
+                $isSafe = $false
+            }
+        }elseif($i -eq ($reports.length - 1)){
+            $safeLines ++
+            $i = 50
+        }else{
+            $lastNumber = [int]$reports[$i]
+            $i++
         }
     }
-    
-    if($Safe){
+   }
+   return $isSafe
+}
+Function New-Arraylist([string]$line,[int]$integer){
+    $newLine = [System.Collections.ArrayList]@()
+    for($i=0; $i -lt $line.length; $i++){
+        if($i -ne $integer){
+            $newLine.Add($value) | Out-Null
+        }
+    }
+    return $newLine
+}
+
+$rawData = Get-Content example.txt
+ <#Safe reports are lines where the numbers are either increasing or decreasing by between 1 and 3 through the whole line
+ but you can remove ONE SINGLE REPORT to render the line safe#>
+$safeLines = 0
+$linesSafeWithChanges = 0
+
+foreach($line in $rawData){
+    if(Compare-Line($line)){
         $safeLines++
-        Write-Host "Line $line is safe"
+        Write-Host "Line $line is safe with no changes"
+    }else{
+        $isSafe = $false
+        $reports = $line.split(" ")
+        for($i = 0; $i -lt $reports.length; $i++){
+            if(!$isSafe){
+                $testLine = New-Arraylist($reports,$i)
+                $isSafe = Compare-Line($testLine)
+            }
+        }
+        if($isSafe){
+            $linesSafeWithChanges++
+        Write-Host "Line $line is safe WITH changes"
+        }
     }
 }
+$totalSafe = $safeLines + $linesSafeWithChanges
+Write-Host "No Changes: $safelines; One change: $linesSafeWithChanges; Total safe: $totalSafe"
