5204493a5ca7249f78aec7353f26f87a936dc12f
Krys <75592214+Kooties@users.noreply.github.com>
2024-12-07 11:20:10 -0500

    Finished Day 7 Part 1 But, uh, don't run the script because I started part 2 And it crashes :D

---
+++ 2024/day7/solver.ps1
@@ -0,0 +1,56 @@
+function Test-Functions($inputData){
+    $numbers = $inputData.split(':').split(' ')
+    $intendedResult = [bigint]$numbers[0]
+    #Write-Host "Starting Test on line $inputData"
+    $resultsArray = [System.Collections.ArrayList]@()
+    $add = [bigint]$numbers[2] + [bigint]$numbers[3]
+    $multiply = [bigint]$numbers[2] * [bigint]$numbers[3]
+    $concat = [string]$numbers[2] + [string]$numbers[3]
+    #Write-Host "First addition sums to $add"
+    #Write-Host "First multiplication results in $multiply"
+    $resultsArray.Add($add) | Out-Null
+    $resultsArray.Add($multiply) | Out-Null
+    $resultsArray.Add($concat) | Out-Null
+    #Write-Host "results array is now" $resultsArray
+    if($numbers.count -gt 4){
+        for($i = 4; $i -lt ($numbers.count); $i++){
+            #Write-Host "Working on operation number" $i-1
+            foreach($value in $resultsArray){
+                $workingArray.Add($value) | out-Null
+            }
+            $resultsArray.clear() | Out-Null
+            #Write-Host "working array is now" $workingArray
+            #$resultsArray.clear() | Out-Null
+            foreach($value in $workingArray){
+                #Write-Host "working on value $value and" $numbers[$i]
+                $add = $value + [bigint]$numbers[$i]
+                $multiply = $value * [bigint]$numbers[$i]
+                $concat = [string]$value + [string]$numbers[$i]
+                #Write-Host "new add is $add and multiply is $multiply"
+                $resultsArray.Add($add) | Out-Null
+                $resultsArray.Add($multiply) | Out-Null
+                $resultsArray.Add($concat) | Out-Null
+                #Write-Host "results array is now" $resultsArray
+            }
+            $workingArray.clear() | Out-Null
+        }
+    }
+    
+    if($resultsArray -contains $intendedResult){
+        return $true
+    }else{
+        return $false
+    }
+}
+
+$rawData = Get-Content input.txt
+$total=0
+foreach($line in $rawData){
+    #Write-Host "Testing line $line"
+    if(Test-Functions($line)){
+        #Write-Host "Line Matches; adding "
+        $total += [bigint]$line.split(':').split(' ')[0]
+    }
+}
+
+Write-Host "total is $total"
