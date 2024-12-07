7fd7e4b20d1a199633be313ceb3da222a993f7bb
Krys <75592214+Kooties@users.noreply.github.com>
2024-12-07 11:46:47 -0500

    Completed part 2 of Day 7

--- 2024/day7/solver.ps1
+++ 2024/day7/solver.ps1
@@ -8,11 +8,13 @@ function Test-Functions($inputData){
     $concat = [string]$numbers[2] + [string]$numbers[3]
     #Write-Host "First addition sums to $add"
     #Write-Host "First multiplication results in $multiply"
+    #Write-Host "First concat results in $concat"
     $resultsArray.Add($add) | Out-Null
     $resultsArray.Add($multiply) | Out-Null
     $resultsArray.Add($concat) | Out-Null
     #Write-Host "results array is now" $resultsArray
     if($numbers.count -gt 4){
+        $workingArray = [System.Collections.ArrayList]@()
         for($i = 4; $i -lt ($numbers.count); $i++){
             #Write-Host "Working on operation number" $i-1
             foreach($value in $resultsArray){
@@ -23,13 +25,19 @@ function Test-Functions($inputData){
             #$resultsArray.clear() | Out-Null
             foreach($value in $workingArray){
                 #Write-Host "working on value $value and" $numbers[$i]
-                $add = $value + [bigint]$numbers[$i]
-                $multiply = $value * [bigint]$numbers[$i]
+                $add = ([bigint]$value + [bigint]$numbers[$i])
+                $multiply = ([bigint]$value * [bigint]$numbers[$i])
                 $concat = [string]$value + [string]$numbers[$i]
-                #Write-Host "new add is $add and multiply is $multiply"
-                $resultsArray.Add($add) | Out-Null
-                $resultsArray.Add($multiply) | Out-Null
-                $resultsArray.Add($concat) | Out-Null
+                #Write-Host "new add is $add, multiply is $multiply, and concat is $concat"
+                if($add -le $intendedResult){
+                    $resultsArray.Add($add) | Out-Null
+                }
+                if($multiply -le $intendedResult){
+                    $resultsArray.Add($multiply) | Out-Null
+                }
+                if([bigint]$concat -le $intendedResult){
+                    $resultsArray.Add($concat) | Out-Null
+                }
                 #Write-Host "results array is now" $resultsArray
             }
             $workingArray.clear() | Out-Null
