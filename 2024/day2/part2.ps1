644d445ebc9acd89d14a4c384d670cc750bee89d
Krys <75592214+Kooties@users.noreply.github.com>
2024-12-08 10:01:23 -0500

    finished part 2 of day 2

--- 2024/day2/part2.ps1
+++ 2024/day2/part2.ps1
@@ -1,78 +1,67 @@
 Function Compare-Line([string]$line){
-    #Write-Host "Parsing next line"
+    ##Write-Host "Parsing next line"
     $lastNumber = 0
     $nextNumber = 0
-    $allowedErrors = 1
     $reports = $line.split(" ")
     $isSafe = $true
     $increasing = $false
     $decreasing = $false
    $i = 0
    while(($i -lt ($reports.length)) -and ($isSafe)){
-    #Write-Host "On value $i"
+    ##Write-Host "On value $i"
     if($i -eq 0){
         $lastNumber = [int]$reports[$i]
         $i++
         #"Value 1 of line is $lastNumber "
     }elseif($i -eq 1){
-        #Write-Host "Parsing second value"
+        ##Write-Host "Parsing second value"
         $nextNumber = [int]$reports[$i]
-        #Write-Host "Last value is $lastNumber"
-        #Write-Host "Next value is $nextNumber"
+        ##Write-Host "Last value is $lastNumber"
+        ##Write-Host "Next value is $nextNumber"
         if($nextNumber -gt $lastNumber){
-            #Write-Host "Line is increasing"
+            ##Write-Host "Line is increasing"
             $increasing = $true
             $decreasing = $false
             if([Math]::Abs($nextNumber - $lastNumber) -gt 3){
-                if($allowedErrors -ne 0){
-                    $allowedErrors--
-                }else{
+                
                     $isSafe = $false
-                }
-                #Write-Host "Jump of more than 3"
+                
+                ##Write-Host "Jump of more than 3"
                 
             }
         }elseif($nextNumber -lt $lastNumber){
-            #Write-Host "Line is decreasing"
+            ##Write-Host "Line is decreasing"
             $decreasing = $true
             $increasing = $false
             if([Math]::Abs($nextNumber - $lastNumber) -gt 3){
-                #Write-Host "Jump of more than 3"
-                if($allowedErrors -ne 0){
-                    $allowedErrors--
-                }else{
+                ##Write-Host "Jump of more than 3"
+                
                     $isSafe = $false
-                }
+                
             }
         }elseif($nextNumber -eq $lastNumber){
-            #Write-Host "Numbers are equal"
-            if($allowedErrors -ne 0){
-                $allowedErrors--
-            }else{
+            ##Write-Host "Numbers are equal"
+            
                 $isSafe = $false
-            }
+            
         }
         $lastNumber = [int]$reports[$i]
         $i++
     }elseif($increasing){
-        #Write-Host "increasing elseif"
+        ##Write-Host "increasing elseif"
         $nextNumber = [int]$reports[$i]
-        #Write-Host "next number is $nextNumber"
+        ##Write-Host "next number is $nextNumber"
         if($nextNumber -le $lastNumber){
-            if($allowedErrors -ne 0){
-                $allowedErrors--
-            }else{
+            
                 $isSafe = $false
-            }
+            
         }elseif([Math]::Abs($nextNumber - $lastNumber) -gt 3){
-            #Write-Host "Jump of more than 3"
-            if($allowedErrors -ne 0){
-                $allowedErrors--
-            }else{
+            ##Write-Host "Jump of more than 3"
+            
                 $isSafe = $false
-            }
+            
         }elseif($i -eq ($reports.length - 1)){
-            #Write-Host "Line is safe"
+            ##Write-Host "Line is safe"
             $safeLines ++
             $i = 50
         }else{
@@ -80,21 +69,17 @@ Function Compare-Line([string]$line){
             $i++
         }
     }elseif($decreasing){
-        #Write-Host "decreasing elseif"
+        ##Write-Host "decreasing elseif"
         $nextNumber = [int]$reports[$i]
-        #Write-Host "next number is $nextNumber"
+        ##Write-Host "next number is $nextNumber"
         if($nextNumber -ge $lastNumber){
-            if($allowedErrors -ne 0){
-                $allowedErrors--
-            }else{
+            
                 $isSafe = $false
-            }
+            
         }elseif([Math]::Abs($nextNumber - $lastNumber) -gt 3){
-            if($allowedErrors -ne 0){
-                $allowedErrors--
-            }else{
+            
                 $isSafe = $false
-            }
+            
         }elseif($i -eq ($reports.length - 1)){
             $safeLines ++
             $i = 50
@@ -106,17 +91,22 @@ Function Compare-Line([string]$line){
    }
    return $isSafe
 }
-Function New-Arraylist([string]$line,[int]$integer){
-    $newLine = [System.Collections.ArrayList]@()
-    for($i=0; $i -lt $line.length; $i++){
-        if($i -ne $integer){
-            $newLine.Add($value) | Out-Null
+
+Function Create-ArrayList([string]$thing,[int]$y){
+    $data = $thing.split(" ")
+    $returnValue = [System.Collections.ArrayList]@()
+    for($x=0; $x -lt $data.count; $x++){
+        #Write-Host "X is $x and Y is $y"
+        if($x -ne $y){
+            $returnValue.Add($data[$x]) | Out-Null
+        }else{
+            ##Write-Host "Removing " $data[$x] " from array"
         }
     }
-    return $newLine
+    return $returnValue
 }
 
-$rawData = Get-Content example.txt
+$rawData = Get-Content input.txt
  <#Safe reports are lines where the numbers are either increasing or decreasing by between 1 and 3 through the whole line
  but you can remove ONE SINGLE REPORT to render the line safe#>
 $safeLines = 0
@@ -125,19 +115,25 @@ $linesSafeWithChanges = 0
 foreach($line in $rawData){
     if(Compare-Line($line)){
         $safeLines++
-        Write-Host "Line $line is safe with no changes"
+        #Write-Host "Line $line is safe with no changes"
     }else{
         $isSafe = $false
-        $reports = $line.split(" ")
-        for($i = 0; $i -lt $reports.length; $i++){
+        $reports = [string]$line.split(" ")
+        $i=0
+        while($i -lt $reports.length){
+            #Write-Host "I is currently $i"
             if(!$isSafe){
-                $testLine = New-Arraylist($reports,$i)
+                $testLine = Create-ArrayList -thing $line -y $i
+                #Write-Host "Checking change to $testLine"
                 $isSafe = Compare-Line($testLine)
+                #Write-Host "$testLine safety is currently $isSafe"
+                $i++
+            }else{
+                $i = $reports.length
+                $linesSafeWithChanges++
+                #Write-Host "Line $line is safe WITH changes"
+                #Write-Host "Changed to $testLine"
             }
-        }
-        if($isSafe){
-            $linesSafeWithChanges++
-        Write-Host "Line $line is safe WITH changes"
         }
     }
 }
